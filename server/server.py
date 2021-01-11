e25142847d3e06201edc5343bb2a731614efbfa1
swaroopsj <66375726+swaroopsj@users.noreply.github.com>
2021-01-11 19:16:21 +0530

    main

--- server/server.py
+++ server/server.py
@@ -44,4 +44,4 @@ if __name__ == '__main__':
     print("starting python flask server for loan status prediction")
     # os.environ.get('PORT',5000)
     # # util.load_saved_artifacts()
-    app.run()
+    app.run(debug=True)
